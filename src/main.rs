474fa54ae2ddd188d17bd44d09bd2ad95cea9aff
Steven Bosnick <sbosnick@sympatico.ca>
2020-07-10 22:44:14 -0400

    Feat(cli): add output option and skeleton for subcommands

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,11 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms
 
-use anyhow::Error;
+use std::path::PathBuf;
+use std::io::{prelude::*, stdout, BufWriter};
+use std::fs::File;
+
+use anyhow::{Context, Error};
 use human_panic::setup_panic;
 use log::Level;
 use loggerv::{Logger, Output};
@@ -18,12 +22,33 @@ struct Opt {
     /// Increases the logging level (use multiple times for more detail).
     #[structopt(short, long, parse(from_occurrences))]
     verbose: u64,
+
+    /// Specifies the output file to use instead of standard out.
+    #[structopt(short, long, parse(from_os_str))]
+    output: Option<PathBuf>,
+
+    #[structopt(subcommand)]
+    subcommand: Subcommand,
+}
+
+#[derive(StructOpt)]
+enum Subcommand {
+    /// Temporary placeholder pending real subcommands.
+    Placeholder,
+}
+
+impl Subcommand {
+    fn run(&self, mut w: impl Write) -> Result<(), Error> {
+        writeln!(w, "Hello World!")?;
+        Ok(())
+    }
 }
 
 fn main() -> Result<(), Error> {
     setup_panic!();
 
     let opt = Opt::from_args();
+
     Logger::new()
         .output(&Level::Trace, Output::Stderr)
         .output(&Level::Debug, Output::Stderr)
@@ -31,7 +56,13 @@ fn main() -> Result<(), Error> {
         .verbosity(opt.verbose)
         .init()?;
 
-    println!("Hello, world!");
+    match opt.output {
+        Some(path) => {
+            let file = File::create(&path)
+                .with_context(|| format!("Failed to create output file {}", path.display()))?;
+            opt.subcommand.run(BufWriter::new(file))
+        }
 
-    Ok(())
+        None => opt.subcommand.run(BufWriter::new(stdout())),
+    }
 }
