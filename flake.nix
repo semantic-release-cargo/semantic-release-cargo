28bb79aab4166ea7c1ef0055b2f1493aabff467c
Eric Crosson <eric.s.crosson@utexas.edu>
2022-12-27 07:32:48 -0600

    chore(nix): filter out non-cargo source packages

--- flake.nix
+++ flake.nix
@@ -39,28 +39,18 @@
 
       # Common derivation arguments used for all builds
       commonArgs = {
-        src = ./.;
+        src = craneLib.cleanCargoSource ./.;
 
         # Add extra inputs here or any other derivation settings
         # doCheck = true;
-        buildInputs = [
-          # openssl
-        ];
+        buildInputs = [];
 
-        nativeBuildInputs = [
-          # pkg-config
-        ];
+        nativeBuildInputs = [];
       };
 
       # Build *just* the cargo dependencies, so we can reuse
       # all of that work (e.g. via cachix) when running in CI
-      cargoArtifacts = craneLib.buildDepsOnly (commonArgs
-        // {
-          # Additional arguments specific to this derivation can be added here.
-          # Be warned that using `//` will not do a deep copy of nested
-          # structures
-          pname = "semantic-release-cargo-deps";
-        });
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
       # Run clippy (and deny all warnings) on the crate source,
       # resuing the dependency artifacts (e.g. from build scripts or
